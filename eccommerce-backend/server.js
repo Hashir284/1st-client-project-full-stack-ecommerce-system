a0bcac2cef0f0db278f42a37ad3bf5dcb4e39e8b
muhammadibsam <onlyforprofile1@gmail.com>
2026-08-21 12:29:06 -0700

    Update server.js

--- eccommerce-backend/server.js
+++ eccommerce-backend/server.js
@@ -1,1 +1,13 @@
+import express from 'express'
+
+const app = express()
+const Port = 9000
+
+app.get('/',(req, res)=>{
+    res.send({status: 'Successful', message:'Mughal Store Api Setup is Ready'})
+})
+
+app.listen(Port, ()=>{
+    console.log('Your app is runnig at port ' + Port);
+})
 
