b9f27889bd60966e9a72283d0df1fccb381efc7c
muhammadibsam <onlyforprofile1@gmail.com>
2026-09-01 07:27:14 -0700

    Update server.js

--- eccommerce-backend/src/server.js
+++ eccommerce-backend/src/server.js
@@ -37,7 +37,7 @@ app.use(
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/',(req, res)=>res.send({status:'successful'}))
+app.get('/',(req, res)=>res.status(200).json({status:'successful'}))
 
 // --- Health check ---
 app.get("/api/health", (req, res) => {
