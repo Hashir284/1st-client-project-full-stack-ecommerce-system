c7620fc6bad22dd634c1c874e23e76fa758219f3
muhammadibsam <onlyforprofile1@gmail.com>
2026-09-02 03:54:37 -0700

    Update server.mjs

--- eccommerce-backend/server.mjs
+++ eccommerce-backend/server.mjs
@@ -22,6 +22,7 @@ app.use(async (req, res, next) => {
     next(error);
   }
 });
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 // --- Core middleware ---
 // const allowedOrigins = (process?.env?.CLIENT_URL || "http://localhost:5173")
@@ -50,7 +51,6 @@ app.get("/api/health", (req, res) => {
 app.get("/", (req, res) => {
   res.status(200).json({ success: true, message: "Welcome to the API" });
 });
-app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 // --- Routes ---
 app.use("/api/auth", authRoutes);
