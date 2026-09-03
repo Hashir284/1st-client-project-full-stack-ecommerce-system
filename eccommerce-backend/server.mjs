484304a7010e751bea8b3519fbb3509ca85e57b5
muhammadibsam <onlyforprofile1@gmail.com>
2026-09-03 05:32:26 -0700

    Update server.mjs

--- eccommerce-backend/server.mjs
+++ eccommerce-backend/server.mjs
@@ -57,7 +57,7 @@ const PORT = process.env.PORT || 4000;
 
 const startServer = async () => {
   await connectDB();
-  if (process.env.NODE_ENV !== 'production') {
+  if (process?.env?.NODE_ENV !== 'production') {
   app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV || "development"} mode on port ${PORT}`);
   })
